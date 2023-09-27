911e104b9832264817d9fdc1ed44711a6ff0640f
jvsenha <joao.santos1381@etec.sp.gov.br>
2023-09-27 15:58:55 -0300

    Cadastrar e alterar

--- apiengerb/src/main/java/br/com/api/apiengerb/controller/ClienteController.java
+++ apiengerb/src/main/java/br/com/api/apiengerb/controller/ClienteController.java
@@ -1,7 +1,11 @@
 package br.com.api.apiengerb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.apiengerb.modelo.ClienteModelo;
@@ -13,7 +17,18 @@ public class ClienteController {
     @Autowired
     private ClienteService cs;
 
-      @GetMapping("/listar")
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ClienteModelo cm){
+        return cs.cadastrarAlterar(cm, "cadastrar");
+
+    }
+     @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ClienteModelo cm){
+        return cs.cadastrarAlterar(cm, "alterar");
+
+    }
+
+    @GetMapping("/listar")
     public Iterable<ClienteModelo> listar(){
         return cs.listar();
     }
--- apiengerb/src/main/java/br/com/api/apiengerb/services/ClienteService.java
+++ apiengerb/src/main/java/br/com/api/apiengerb/services/ClienteService.java
@@ -1,21 +1,45 @@
 package br.com.api.apiengerb.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.apiengerb.modelo.ClienteModelo;
+import br.com.api.apiengerb.modelo.RespostaModelo;
 import br.com.api.apiengerb.repositório.ClienteRepositorio;
 
 @Service
 public class ClienteService {
-    
+
     @Autowired
     private ClienteRepositorio cr;
 
-    
-    //Método para listar todos os clientes
-    public Iterable <ClienteModelo> listar(){
+    //
+    @Autowired
+    private RespostaModelo rm;
+
+    // Método para listar todos os clientes
+    public Iterable<ClienteModelo> listar() {
         return cr.findAll();
     }
-    
+
+    // Metodo para cadastrar ou alterar Clientes
+    public ResponseEntity<?> cadastrarAlterar(ClienteModelo cm, String acao) {
+
+        if (cm.getNomeCliente().equals("")) {
+            rm.setMensagem("O nome do cliente é obrigatório");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        } else if (cm.getEmailCliente().equals("")) {
+            rm.setMensagem("O email do cliente é obrigatório");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        } else {
+            if (acao.equals("cadastrar")) {
+                return new ResponseEntity<ClienteModelo>(cr.save(cm), HttpStatus.CREATED);
+            }else{
+                 return new ResponseEntity<ClienteModelo>(cr.save(cm), HttpStatus.OK);
+            }
+        }
+
+    }
 }
